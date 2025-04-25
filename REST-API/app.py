cd03f39efec0c32c955302991cf6f80e50a90b24
Misko2409 <miskovic224@gmail.com>
2025-04-25 05:07:38 +0200

    REST API Updatean

--- REST-API/app.py
+++ REST-API/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from joblib import load
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)  #omogućuje CORS za sve rute
 
 # Učitaj model i label encoder
 model = load("../TrainingModel/models/weather_classifier_model.pkl")
